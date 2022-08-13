96d564afb7b5c37a9c058c089d36542ee1a18f16
gcarvellas <gcarvellas@castlepointanime.com>
2022-08-12 23:48:57 -0400

    fixed STL renderer

--- public/js/stl_drag_and_drop.js
+++ public/js/stl_drag_and_drop.js
@@ -23,7 +23,9 @@ dropZone.addEventListener('drop', function(e) {
         
         const reader = new FileReader();
         reader.onload = function(e2) {
-            render(convertDataURItoArrayBuffer(e2.target.result)); //TODO convert to binary
+            convertDataURItoBlob(e2.target.result).text().then(value => 
+                render(value.replace(/(\r\n|\n|\r)/gm, ""))
+                );
         }
 
         reader.readAsDataURL(file);
@@ -42,17 +44,29 @@ function hideError(){
     
 }
 
-function convertDataURItoArrayBuffer(dataURI){
-    var byteString = atob(dataURI.split(',')[1]);
+function convertDataURItoBlob(dataURI){
 
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  //https://www.codegrepper.com/code-examples/javascript/convert+dataurl+to+string+and+back+to+dataurl
 
-    // write the bytes of the string to an ArrayBuffer
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    return ab;
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
 }
--- public/js/stlviewer.js
+++ public/js/stlviewer.js
@@ -2,87 +2,30 @@ import * as THREE from 'three';
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 import {STLLoader} from "https://unpkg.com/three@0.143.0/examples/jsm/loaders/STLLoader.js";
 
-function test_cube(){
-
-    const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
-
-    const scene = new THREE.Scene();
-
-    const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    const material = new THREE.MeshNormalMaterial();
-
-    const mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
-
-    const renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setAnimationLoop( animation );
-    document.body.appendChild( renderer.domElement );
-
-    // animation
-
-    function animation( time ) {
-
-        mesh.rotation.x = time / 2000;
-        mesh.rotation.y = time / 1000;
-
-        renderer.render( scene, camera );
-
-    }
-}
-
 export function render(modelBinary){
-    const elem = document.getElementById("stlviewer");
-    var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    var camera = new THREE.PerspectiveCamera(70, elem.clientWidth / elem.clientHeight, 1, 1000);
-    renderer.setSize(elem.clientWidth, elem.clientHeight);
-    elem.appendChild(renderer.domElement);
 
-    window.addEventListener('resize', function () {
-        renderer.setSize(elem.clientWidth, elem.clientHeight);
-        camera.aspect = elem.clientWidth / elem.clientHeight;
-        camera.updateProjectionMatrix();
-    }, false);
-
-    var controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.rotateSpeed = 0.05;
-    controls.dampingFactor = 0.1;
-    controls.enableZoom = false;
-    controls.enablePan = false;
-    controls.autoRotate = true;
-    controls.autoRotateSpeed = .75;
-
+    //https://codepen.io/leonnicklas/pen/jOyqXoN
+    
     var scene = new THREE.Scene();
-
-    scene.add(new THREE.HemisphereLight(0xffffff, 0x080820, 1.5));
-
-    (new STLLoader()).parse(modelBinary, function (geometry) {
-        var material = new THREE.MeshPhongMaterial({ color: 0xff5533, specular: 100, shininess: 100 });
-        var mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
-
-        // Compute the middle
-        var middle = new THREE.Vector3();
-        geometry.computeBoundingBox();
-        geometry.boundingBox.getCenter(middle);
-
-        // Center it
-        mesh.position.x = -1 * middle.x;
-        mesh.position.y = -1 * middle.y;
-        mesh.position.z = -1 * middle.z;
-
-        // Pull the camera away as needed
-        var largestDimension = Math.max(geometry.boundingBox.max.x,
-            geometry.boundingBox.max.y, geometry.boundingBox.max.z)
-        camera.position.z = largestDimension * 1.5;
-
-
-        var animate = function () {
-            requestAnimationFrame(animate);
-            controls.update();
-            renderer.render(scene, camera);
-        }; animate();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.setScalar(0.5);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    var controls = new OrbitControls(camera, renderer.domElement);
+    
+    var loader = new STLLoader();
+    
+    var geometry = loader.parse(modelBinary); // change "testAscii" to "testBinary" and it will not work.
+    console.log(geometry);
+    
+    var material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    
+    
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera)
     });
 }
